635216d5789e558d9ab5865a366004f9d2ec3288
kimzes <117077471+wonder002@users.noreply.github.com>
2024-07-17 19:12:21 +0900

    initial commit

---
+++ src/test/java/com/umc/taximate/TaximateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umc.taximate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaximateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
